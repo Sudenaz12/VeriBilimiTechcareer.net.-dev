a69c4ed3fed2f87c4b89c24c958dcdfb7654f6c5
Sudenaz <odemissudenaz75@gmail.com>
2025-10-31 18:33:15 +0300

    Add files via upload

---
+++ ödev1.py
@@ -0,0 +1,12 @@
+isim = "Sudenaz"       # string
+yas = 20               # integer
+boy = 1.75               # float
+departman="computer engineering"    
+city ="giresun"
+university ="Yalova üniversitesi"
+grade= "2.sinif"         
+
+# Tek print komutuyla tanıtım cümlesi
+print(f"Merhaba! Ben {isim}, {yas} yaşindayim, boyum {boy} metre. "
+      f"{university}’nde {departman} bölümünde {grade} öğrencisiyim ve {city} şehrinde yaşiyorum.")
+
